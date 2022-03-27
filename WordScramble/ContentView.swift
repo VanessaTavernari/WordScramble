14c8d92a30fae800992ef3125a255172fd2fd964
Vanessa Tavares Tavernari <vanessa.tavernari@gmail.com>
2022-03-27 23:56:13 +0100

    Initial commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,138 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Vanessa T Tavernari on 18/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var usedWords  = [String] ()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    @State private var showingScore = false
+    @State private var scoreGame: Int = 0
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                    .padding()
+
+                List(usedWords, id: \.self) {
+                    Text($0)
+                }
+
+                Text("Score: \(scoreGame)")
+
+                Button(action: {
+                    self.endGame()
+                }) {
+                    Text("New Game")
+                        .padding()
+                }
+            }
+           
+            .navigationBarTitle(rootWord)
+            .onAppear(perform: startGame)
+            .alert(isPresented: $showingError) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("Ok")))
+            }
+        }
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else {
+            return
+        }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not possible", message: "That isn't a real word.")
+            return
+        }
+        
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+        scoreGame += 2
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allwords = startWords.components(separatedBy: "\n")
+                
+                rootWord = allwords.randomElement() ?? "silkworm"
+                scoreGame = 0
+                return
+            }
+        }
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+
+    func isReal(word: String) -> Bool {
+        guard word.count > 2 else {
+            return false
+        }
+        
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+        
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
+    func endGame() {
+        wordError(title: "End Game", message: "\(usedWords.count <= 1 ? "\(usedWords.count) word":" \(usedWords.count) words") - \(scoreGame) points.")
+        usedWords.removeAll()
+        newWord.removeAll()
+        startGame()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Vanessa T Tavernari on 18/12/20.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
